a25531902b6aa837e08541d64c606897d51d6ee0
jatkinson1000 <109271713+jatkinson1000@users.noreply.github.com>
2023-10-22 08:55:09 +0100

    Remove comments and redundant else clause from copy button.

--- static/js/copy-code-button.js
+++ static/js/copy-code-button.js
@@ -33,14 +33,4 @@ function addCopyButtons(clipboard) {
 
 if (navigator && navigator.clipboard) {
     addCopyButtons(navigator.clipboard);
-} else {
-    // var script = document.createElement('script');
-    // script.src = 'https://cdnjs.cloudflare.com/ajax/libs/clipboard-polyfill/2.7.0/clipboard-polyfill.promise.js';
-    // script.integrity = 'sha256-waClS2re9NUbXRsryKoof+F9qc1gjjIhc2eT7ZbIv94=';
-    // script.crossOrigin = 'anonymous';
-    // script.onload = function() {
-    //     addCopyButtons(clipboard);
-    // };
-
-    // document.body.appendChild(script);
 }
